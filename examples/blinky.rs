81cd5c5f88c0c2503ce24c0c607446c222205877
sam blenny <68084116+samblenny@users.noreply.github.com>
2026-01-22 17:14:45 +0000

    rust fmt

--- examples/blinky.rs
+++ examples/blinky.rs
@@ -1,8 +1,6 @@
 #![no_std]
 #![no_main]
 /// Goal: Blink an LED on one of the GPIO pins
-
-
 extern crate dabao_baremetal_poc;
 
 /// Rust entry point: _start() calls main()
--- src/lib.rs
+++ src/lib.rs
@@ -10,13 +10,13 @@ use core::arch::asm;
 use core::panic::PanicInfo;
 
 unsafe extern "C" {
-	fn _idata_start(); // Start .data in FLASH (ReRAM)
-	fn _data_start();  // Start .data in SRAM
-	fn _data_end();    // End   .data in SRAM
-	fn _bss_start();   // Start .bss  in SRAM
-	fn _bss_end();     // End   .bss  in SRAM
-	fn _stack_end();
-	fn main() -> !;
+    fn _idata_start(); // Start .data in FLASH (ReRAM)
+    fn _data_start(); // Start .data in SRAM
+    fn _data_end(); // End   .data in SRAM
+    fn _bss_start(); // Start .bss  in SRAM
+    fn _bss_end(); // End   .bss  in SRAM
+    fn _stack_end();
+    fn main() -> !;
 }
 
 /// Boot entry point (bootloader jumps here)
@@ -34,30 +34,28 @@ pub extern "C" fn _start() -> ! {
     }
 
     init();
-	unsafe {
-		main();
-	}
+    unsafe {
+        main();
+    }
 }
 
 /// Copy .data and zero .bss
 fn init() {
     unsafe {
-
         // Copy .data section from FLASH to RAM
         let data_start = _data_start as *mut u8;
         let data_end = _data_end as *mut u8;
         let flash_start = _idata_start as *const u8;
-		let size = data_end as usize - data_start as usize;
-		core::ptr::copy_nonoverlapping(flash_start, data_start, size);
+        let size = data_end as usize - data_start as usize;
+        core::ptr::copy_nonoverlapping(flash_start, data_start, size);
 
         // Zero the .bss section
         let bss_start = _bss_start as *mut u8;
         let bss_end = _bss_end as *mut u8;
-		let size = bss_end as usize - bss_start as usize;
-		core::ptr::write_bytes(bss_start, 0, size);
+        let size = bss_end as usize - bss_start as usize;
+        core::ptr::write_bytes(bss_start, 0, size);
     }
 }
-
 
 /// Panic Handler for no_std.
 #[panic_handler]
